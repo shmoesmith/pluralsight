19818d66d81e994799b9ed42dcad664548ad0045
Alan Vincent <alan.d.vincent@gmail.com>
2017-11-20 15:45:58 -0700

    comments for clarity

--- app/challenge.js
+++ app/challenge.js
@@ -1,23 +1,28 @@
 let packages = ["sponge: ", "bob: ", "pants: square", "square: sponge", "star: bob" ];
-let arrayA = []
-let arrayB = []
-let arrayC = []
+let arrayA = [] //the final list
+let arrayB = [] //list of stuff with dependencies
+let arrayC = [] //list b parsed so we can test the dependencies against list A
 
 var installer = (packageList) => {
+  //add to the final list things we know can immediately go on it
   for (var package of packageList) {
     loopArray = package.split(' ');
     if(loopArray.includes("")) {
+      //stuff we know can go in it immediately
       arrayA.push(package)
     } else {
+      //stuff that needs to be parsed and tested against known good packages
       arrayB.push(package)
     };
   };
 
+//parse out package so we can look for dependency and check if already installed
   for (var package of arrayB) {
   insideArray = package.split(':');
   arrayC.push([insideArray])
   }
 
+//test parsed out dependency against list and push it if 'ok' and remove it from parsed list
   for (var package of arrayC) {
      var packageIndex = arrayC.indexOf(package)
      var dependency = (package[0][1]).trim()
