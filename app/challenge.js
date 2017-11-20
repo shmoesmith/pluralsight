d88da15d22dab7dc631c66af77df4fcce8faa914
Alan Vincent <alan.d.vincent@gmail.com>
2017-11-20 02:27:22 -0700

    created new array that combines packages without dependencies

--- app/challenge.js
+++ app/challenge.js
@@ -1,8 +1,14 @@
-let packages = ["test", "array"];
+let packages = ["sponge: ", "bob: ", "square: sponge" ];
+let arrayA = []
 
 var installer = (packageList) => {
-
+  for (let package of packageList) {
+    loopArray = package.split(' ')
+    if(loopArray.includes("")) {
+      arrayA.push(package)
+    }
+  }
 }
-
 module.exports.packages = packages;
 module.exports.installer = installer;
+module.exports.arrayA = arrayA;
--- test/challenge_test.js
+++ test/challenge_test.js
@@ -4,15 +4,21 @@ var expect = chai.expect;
 var challenge = require('../app/challenge.js');
 var installer = challenge.installer;
 var packages = challenge.packages;
+let arrayA = challenge.arrayA;
 
 describe('Challenge', () => {
   it('makes sure the function exists', () => {
     expect(installer).to.exist;
   });
 
-  it('makes sure packages are an array', () => {
+  it('makes sure packages is an array', () => {
     expect(packages).to.be.an('array')
   });
 
+  it('should return packages with no dependecy into a new array', () => {
+    let testArray = ["sponge: ", "bob: ", "square: sponge" ];
+    installer(testArray);
+    expect(arrayA).to.have.lengthOf(2);
+  })
 
 });
