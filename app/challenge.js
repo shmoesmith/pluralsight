5b3f9d96cbf0ac1a1af4fe9170b66184eb00bc73
Alan Vincent <alan.d.vincent@gmail.com>
2017-11-20 10:20:45 -0700

    split dependcies to array of array for future testing

--- app/challenge.js
+++ app/challenge.js
@@ -1,19 +1,25 @@
 let packages = ["sponge: ", "bob: ", "pants: square", "square: sponge" ];
 let arrayA = []
 let arrayB = []
+let arrayC = []
 
 var installer = (packageList) => {
-  for (let package of packageList) {
-    loopArray = package.split(' ')
+  for (var package of packageList) {
+    loopArray = package.split(' ');
     if(loopArray.includes("")) {
       arrayA.push(package)
     } else {
       arrayB.push(package)
-    }
+    };
+  };
+  for (var package of arrayB) {
+  insideArray = package.split(':');
+  arrayC.push([insideArray])
   }
-}
+};
 
 module.exports.packages = packages;
 module.exports.installer = installer;
 module.exports.arrayA = arrayA;
 module.exports.arrayB = arrayB;
+module.exports.arrayC = arrayC;
--- test/challenge_test.js
+++ test/challenge_test.js
@@ -6,6 +6,7 @@ var installer = challenge.installer;
 var packages = challenge.packages;
 let arrayA = challenge.arrayA;
 let arrayB = challenge.arrayB;
+let arrayC = challenge.arrayC
 
 describe('Challenge', () => {
   it('makes sure the function exists', () => {
@@ -16,7 +17,7 @@ describe('Challenge', () => {
     expect(packages).to.be.an('array')
   });
 
-  it('should return packages with no dependecy into a new array', () => {
+  it('should return packages with no dependency into a new array', () => {
     const testArray = ["sponge: ", "bob: ", "square: sponge" ];
     installer(testArray);
     expect(arrayA).to.have.lengthOf(2);
@@ -27,5 +28,11 @@ describe('Challenge', () => {
     installer(testArray2);
     expect(arrayB).to.have.lengthOf(2);
   })
+
+  it('breaks up array b into an array of arrays based on the :', () => {
+    const testArray2 = ["sponge: ", "bob: ", "pants: square", "square: sponge"];
+    installer(testArray2)
+   expect(arrayC[0][0]).to.have.lengthOf(2)
+ })
 
 });
