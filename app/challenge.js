b82ea48a10838dc0d9554b3eba77ca57ed2b1933
Alan Vincent <alan.d.vincent@gmail.com>
2017-11-20 01:14:59 -0700

    first 2 test to try out TDD with mocha and chai

---
+++ app/challenge.js
@@ -0,0 +1,8 @@
+let packages = ["test", "array"];
+
+var installer = (packageList) => {
+
+}
+
+module.exports.packages = packages;
+module.exports.installer = installer;
--- test/challenge_test.js
+++ test/challenge_test.js
@@ -1,2 +1,18 @@
 var chai = require('chai');
 var assert = chai.assert;
+var expect = chai.expect;
+var challenge = require('../app/challenge.js');
+var installer = challenge.installer;
+var packages = challenge.packages;
+
+describe('Challenge', () => {
+  it('makes sure the function exists', () => {
+    expect(installer).to.exist;
+  });
+
+  it('makes sure packages are an array', () => {
+    expect(packages).to.be.an('array')
+  });
+
+
+});
